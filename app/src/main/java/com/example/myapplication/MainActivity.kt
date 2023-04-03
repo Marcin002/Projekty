2ca49ec3fe779ba93eb6c33f9f8a9aaaf6be9bf7
mihau11 <90836156+mihau11@users.noreply.github.com>
2023-04-03 15:34:37 +0200

    isVisible

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.RadioButton
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity() {
 
         val dodawanie=findViewById<RadioButton>(R.id.radio1)
         val odejmowanie=findViewById<RadioButton>(R.id.radio2)
+
+        dodawanie.setOnClickListener {
+            findViewById<CheckBox>(R.id.checkBox).isVisible=true
+        }
+        odejmowanie.setOnClickListener {
+            findViewById<CheckBox>(R.id.checkBox).isVisible=false
+        }
     }
 }
