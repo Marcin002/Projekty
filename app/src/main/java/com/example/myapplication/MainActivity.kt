c8298b524482ffd7ffddf70fe4d5612b8d985877
mihau11 <90836156+mihau11@users.noreply.github.com>
2023-04-03 15:19:52 +0200

    checkbox

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dodawanie=findViewById<RadioButton>(R.id.radio1)
+        val odejmowanie=findViewById<RadioButton>(R.id.radio2)
     }
 }
