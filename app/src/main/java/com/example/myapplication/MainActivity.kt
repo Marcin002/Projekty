6a8449b9f8c9e8388f732c243117adf253d23131
Marcin <marcinkummer99@gmail.com>
2023-04-15 17:01:44 +0200

    chip group

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         val dodawanie=findViewById<RadioButton>(R.id.radio1)
         val odejmowanie=findViewById<RadioButton>(R.id.radio2)
         val chip=findViewById<ChipGroup>(R.id.chipGroup)
+        val chipg2=findViewById<ChipGroup>(R.id.chipGroup2)
+        val chipg3=findViewById<ChipGroup>(R.id.chipGroup3)
 
         val check1=findViewById<CheckBox>(R.id.checkBox)
         val check2=findViewById<CheckBox>(R.id.checkBox2)
@@ -77,6 +79,8 @@ class MainActivity : AppCompatActivity() {
             check9.isVisible=true
             check10.isVisible=true
             chip.isVisible=false
+            chipg2.isVisible=false
+            chipg3.isVisible=false
         }
         odejmowanie.setOnClickListener {
             check1.isVisible=false
@@ -92,17 +96,47 @@ class MainActivity : AppCompatActivity() {
 
 
             chip.isVisible=true
-            if(check1.isChecked==true){
-                chip1.isVisible=true
+            chipg2.isVisible=true
+            chipg3.isVisible=true
+            when(check1.isChecked){
+                true ->{ chip1.isVisible = true }
+                false ->{ chip1.isVisible = false }
             }
-            if(check2.isChecked==true){
-                chip2.isVisible=true
+            when(check2.isChecked){
+                true ->{ chip2.isVisible = true }
+                false ->{ chip2.isVisible = false }
             }
-            if(check3.isChecked==true){
-                chip3.isVisible=true
+            when(check3.isChecked){
+                true ->{ chip3.isVisible = true }
+                false ->{ chip3.isVisible = false }
             }
-            if(check4.isChecked==true){
-                chip4.isVisible=true
+            when(check4.isChecked){
+                true ->{ chip4.isVisible = true }
+                false ->{ chip4.isVisible = false }
+            }
+            when(check5.isChecked){
+                true ->{ chip5.isVisible = true }
+                false ->{ chip5.isVisible = false }
+            }
+            when(check6.isChecked){
+                true ->{ chip6.isVisible = true }
+                false ->{ chip6.isVisible = false }
+            }
+            when(check7.isChecked){
+                true ->{ chip7.isVisible = true }
+                false ->{ chip7.isVisible = false }
+            }
+            when(check8.isChecked){
+                true ->{ chip8.isVisible = true }
+                false ->{ chip8.isVisible = false }
+            }
+            when(check9.isChecked){
+                true ->{ chip9.isVisible = true }
+                false ->{ chip9.isVisible = false }
+            }
+            when(check10.isChecked){
+                true ->{ chip10.isVisible = true }
+                false ->{ chip10.isVisible = false }
             }
         }
     }
