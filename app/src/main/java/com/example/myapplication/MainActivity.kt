4754ad0e4ee587cb761139e6f139e95ef63049fb
Marcin <marcinkummer99@gmail.com>
2023-04-15 17:04:46 +0200

    chip ischecked

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -99,43 +99,63 @@ class MainActivity : AppCompatActivity() {
             chipg2.isVisible=true
             chipg3.isVisible=true
             when(check1.isChecked){
-                true ->{ chip1.isVisible = true }
+                true ->{ chip1.isVisible = true
+                         chip1.isChecked=true
+                }
                 false ->{ chip1.isVisible = false }
             }
             when(check2.isChecked){
-                true ->{ chip2.isVisible = true }
+                true ->{ chip2.isVisible = true
+                         chip2.isChecked=true
+                }
                 false ->{ chip2.isVisible = false }
             }
             when(check3.isChecked){
-                true ->{ chip3.isVisible = true }
+                true ->{ chip3.isVisible = true
+                         chip3.isChecked=true
+                }
                 false ->{ chip3.isVisible = false }
             }
             when(check4.isChecked){
-                true ->{ chip4.isVisible = true }
+                true ->{ chip4.isVisible = true
+                         chip4.isChecked=true
+                }
                 false ->{ chip4.isVisible = false }
             }
             when(check5.isChecked){
-                true ->{ chip5.isVisible = true }
+                true ->{ chip5.isVisible = true
+                         chip5.isChecked=true
+                }
                 false ->{ chip5.isVisible = false }
             }
             when(check6.isChecked){
-                true ->{ chip6.isVisible = true }
+                true ->{ chip6.isVisible = true
+                         chip6.isChecked=true
+                }
                 false ->{ chip6.isVisible = false }
             }
             when(check7.isChecked){
-                true ->{ chip7.isVisible = true }
+                true ->{ chip7.isVisible = true
+                         chip7.isChecked=true
+                }
                 false ->{ chip7.isVisible = false }
             }
             when(check8.isChecked){
-                true ->{ chip8.isVisible = true }
+                true ->{ chip8.isVisible = true
+                         chip8.isChecked=true
+                }
                 false ->{ chip8.isVisible = false }
             }
             when(check9.isChecked){
-                true ->{ chip9.isVisible = true }
+                true ->{ chip9.isVisible = true
+                         chip9.isChecked=true
+                }
                 false ->{ chip9.isVisible = false }
             }
             when(check10.isChecked){
-                true ->{ chip10.isVisible = true }
+                true ->{ chip10.isVisible = true
+                         chip10.isChecked=true
+                }
                 false ->{ chip10.isVisible = false }
             }
         }
